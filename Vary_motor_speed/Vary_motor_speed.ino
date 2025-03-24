ce181a8c616019f05a64f00b57f687e8f0f10e4d
Bukunmi Ayilola <oluwabukunmi.ayilola@gmail.com>
2025-03-24 16:18:15 +0000

    Set the torque level using pwm
    This sketch sets a torque based on pwm signals

---
+++ Vary_motor_speed/Vary_motor_speed.ino
@@ -0,0 +1,34 @@
+#include "esp32-hal-ledc.h"
+
+const int motorPwmPin_power = 17;  
+const int motorPwmPin_speed = 18;
+const int motorDirectionPin = 16;
+const int pwmChannel  = 0;  
+const int pwmFreq     = 20000; 
+const int pwmResolution = 8;   
+
+void setup(){
+    // Configure pin mode for power pin
+  pinMode(motorDirectionPin, OUTPUT);
+  digitalWrite(motorDirectionPin, LOW);
+  
+  // Configure pin mode for power pin
+  pinMode(motorPwmPin_power, OUTPUT);
+  digitalWrite(motorPwmPin_power, HIGH);
+
+  // Setup PWM channel and attach the speed control pin
+  ledcAttach(motorPwmPin_speed, pwmFreq, pwmResolution);
+}
+
+void loop(){
+  // Increase the brightness (or motor speed)
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    ledcWrite(pwmChannel, dutyCycle);
+    delay(15);
+  }
+  // Decrease the brightness (or motor speed)
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+    ledcWrite(pwmChannel, dutyCycle);   
+    delay(15);
+  }
+}
